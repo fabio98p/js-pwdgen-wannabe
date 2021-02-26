f0acbf3216b0dbe3b913af64c826d58b8d50893d
fabio <parisifabio98@gmail.com>
2021-02-26 15:26:32 +0100

    js pw-gen v1.0 completato

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+//controllo se il js parte
+var hello = ("inizio");
+console.log(hello);
+
+//chiedo al utente di inserire un nome
+var name = prompt("Please enter your name", "ginopippo");
+console.log(name);
+
+//chiedo al utente di inserire un cognome
+var secondName = prompt("Please enter your second name", "fungo");
+console.log(secondName);
+
+//chiedo all'utente di inserire un colore
+var color = prompt("Please enter your favorite color", "marrone");
+console.log(color);
+
+//creo una password in cui c'è la combinazione del nome cognome e colore con in piu l'anno corrente
+var password = "your new password is: " + name + secondName + color + 21;
+
+//inserisco la password nel html
+document.getElementById("newPassword").innerHTML = password;
